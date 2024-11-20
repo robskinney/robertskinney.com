cd14b74822074a46fe1945847506880d50aa9d2e
Robert Kinney <robertskinney@outlook.com>
2024-11-20 11:20:25 -0500

    fix: fixed tailwindcss error

--- config/fonts.ts
+++ config/fonts.ts
@@ -4,3 +4,8 @@ export const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
+
+export const fontMono = FontMono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+});
