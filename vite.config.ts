772f269c85ed7674e185be119e161f6803b0127e
Robert Kinney <robertskinney@outlook.com>
2025-10-15 17:12:06 -0400

    feat: added nitro

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,22 @@
 import { defineConfig } from "vite";
-import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import contentCollections from "@content-collections/vite";
+import viteTsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
+import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
 
 export default defineConfig({
   server: {
     port: 3000,
   },
   plugins: [
-    tsConfigPaths({
+    nitroV2Plugin(),
+    // this is the plugin that enables path aliases
+    viteTsConfigPaths({
       projects: ["./tsconfig.json"],
     }),
+    tailwindcss(),
     tanstackStart(),
     viteReact(),
     contentCollections(),
