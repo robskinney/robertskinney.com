597fba5cd64457d329cf4c8f20f9f25e3296c26b
Robert Kinney <robertskinney@outlook.com>
2026-01-31 15:45:21 -0500

    feat: added project pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [mdx()],
 });
---
+++ src/content.config.ts
@@ -0,0 +1,17 @@
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
+
+const projects = defineCollection({
+  loader: glob({ pattern: "**/*.mdx", base: "./src/content/projects" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    publishedAt: z.date(),
+    link: z.string().url(),
+    image: z.string(),
+    tags: z.array(z.string()),
+  }),
+});
+
+export const collections = { projects };
